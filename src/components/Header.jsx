917ca0ff991bf9e622455cb77f37da79137b36b3
R-Saunders <richsaunders@protonmail.com>
2022-10-12 19:15:32 +0100

    added png image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,10 @@ export default function Header(){
   return(
     <React.Fragment>
       <header>
-        <h2>Bestiary</h2>
+        <div>
+          {/* <img src="../imgs/Bestiary.png"/> */}
+          <h2>Bestiary</h2>
+        </div>
         <div>
           <button>Random Article</button>
           <input type="text" placeholder="Search"/>
