57447108156fabbcb03db30961827fcd32bb6b62
R-Saunders <richsaunders@protonmail.com>
2022-10-12 19:30:04 +0100

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,20 @@ import React from "react";
 export default function Footer(){
   return(
     <footer>
-      <button>Support</button>
       <div>
-        <i class="fa-brands fa-square-facebook"></i>
-        <i class="fa-brands fa-square-instagram"></i>
-        <i class="fa-brands fa-square-twitter"></i>
+        <button>Support</button>
+        <div>
+          <i class="fa-brands fa-square-facebook"></i>
+          <i class="fa-brands fa-square-instagram"></i>
+          <i class="fa-brands fa-square-twitter"></i>
+        </div>
+      </div>
+      <div>
+        <p>
+          Made by R Saunders
+        </p>
+        <i class="fa-brands fa-square-github"></i>
+        <i class="fa-brands fa-linkedin"></i>
       </div>
     </footer>
   )
