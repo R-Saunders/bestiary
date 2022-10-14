5da70fa16bdbdbe7f7f8343cb8fd620dcecdcee5
R-Saunders <richsaunders@protonmail.com>
2022-10-14 19:49:19 +0100

    added random article button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import Logo from '../imgs/Bestiary.png'
+import Logo from "../imgs/Bestiary.png";
+import RandomBtn from "./RandomBtn";
 
-export default function Header(){
-  return(
-    <React.Fragment>
-      <header>
-        <div>
-          <img src={Logo} alt="Bestiary Logo" />
-          <h2>Bestiary</h2>
-        </div>
-        <div>
-          <button>Random Article</button>
-          <input type="text" placeholder="Search"/>
-        </div>
-      </header>
-    </React.Fragment>
-  )
+export default function Header() {
+	return (
+		<React.Fragment>
+			<header>
+				<div>
+					<img src={Logo} alt="Bestiary Logo" />
+					<h2>Bestiary</h2>
+				</div>
+				<div>
+					<RandomBtn />
+					<input type="text" placeholder="Search" />
+				</div>
+			</header>
+		</React.Fragment>
+	);
 }
---
+++ src/components/RandomBtn.jsx
@@ -0,0 +1,5 @@
+const RandomBtn = () => {
+	return <button>Random Article</button>;
+};
+
+export default RandomBtn;
