182f8c09c75de08f4fbe35560fd6d2905595f194
R-Saunders <richsaunders@protonmail.com>
2022-10-14 19:50:33 +0100

    refactored blurb to use div instead of react fragment to give better structure

--- src/components/Blurb.jsx
+++ src/components/Blurb.jsx
@@ -1,19 +1,17 @@
-import Desc from './BeastDesc';
-import Data from '../data/beasts.json'
-import Name from './BeastName';
-import React from 'react';
+import Desc from "./BeastDesc";
+import Data from "../data/beasts.json";
+import Name from "./BeastName";
+import React from "react";
 
-export default function Blurb(){
-  return(
-    <div>
-      { Data.beasts.map(
-        (props, index) => (
-          <React.Fragment>
-            <Name key={index} props={props}/>
-            <Desc key={index} props={props}/>
-          </React.Fragment>
-        )
-      )}
-    </div>
-  )
+export default function Blurb() {
+	return (
+		<section>
+			{Data.beasts.map((props, index) => (
+				<div>
+					<Name key={index} props={props} />
+					<Desc key={index} props={props} />
+				</div>
+			))}
+		</section>
+	);
 }
