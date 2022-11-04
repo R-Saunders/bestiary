79467ac3dfda03e69d32ab09132762ae8df4f55a
R-Saunders <richsaunders@protonmail.com>
2022-11-04 21:00:51 +0000

    update

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-//import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Entry from './pages/Entry';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
+const App = () => {
+  return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -18,6 +17,12 @@ root.render(
         </Route>
       </Routes>
     </BrowserRouter>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App/>
 );
 
 // If you want to start measuring performance in your app, pass a function
